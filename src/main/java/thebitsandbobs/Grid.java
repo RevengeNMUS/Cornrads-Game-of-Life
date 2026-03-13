dbbb2a0b2d976a8afeaefebb591b149d9f66b6b1
Nathan M <nathanm5@ohs.stanford.edu>
2026-03-13 04:04:31 -0700

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- src/main/java/thebitsandbobs/Grid.java
+++ src/main/java/thebitsandbobs/Grid.java
@@ -1,4 +1,41 @@
 package thebitsandbobs;
 
+import java.util.Arrays;
+
 public class Grid {
+    private Life[][] lifeGrid;
+
+    public Grid() {
+        //default 3x3 because susus amojgus
+        lifeGrid =
+                new Life[][]{
+                        {new Life(), new Life(), new Life()},
+                        {new Life(), new Life(), new Life()},
+                        {new Life(), new Life(), new Life()}
+        };
+    }
+
+    private Grid(Life[][] lifeGrid) {
+        this.lifeGrid = lifeGrid;
+    }
+
+    public static Grid gridOf(Life[][] lifeArray) {
+        return new Grid(lifeArray);
+    }
+
+    public Life[][] update() {
+        return null;
+    }
+
+    public Life[][] getLifeGrid() {
+        return lifeGrid;
+    }
+
+    @Override
+    public String toString() {
+        return "Grid{" +
+                "lifeGrid=" + Arrays.toString(lifeGrid) +
+                '}';
+        //MAKE ACTUAL THING
+    }
 }
--- src/main/java/thebitsandbobs/Life.java
+++ src/main/java/thebitsandbobs/Life.java
@@ -1,4 +1,37 @@
 package thebitsandbobs;
 
 public class Life {
+    private boolean state;
+
+    public Life() {
+        state = false;
+    }
+
+    public Life(boolean initState) {
+        state = initState;
+    }
+
+    //NO MORE CONTSURCTORS BC WE HATE THEM TEHY SUCK DIE
+
+    public void kill() {
+        state = false;
+    }
+
+    public void live() {
+        state = true;
+    }
+
+    public void setState(boolean life) {
+        state = life;
+    }
+
+    public static boolean getUpdatedState(Grid grid, int x, int y) {
+        //DO SMTH FRFR HERE YOU CHUNGUS
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return state ? "🌽" : "🌌";
+    }
 }
